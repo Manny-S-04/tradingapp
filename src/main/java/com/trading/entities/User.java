450ec30ed0f1059560650538b356df068f44f18a
manny <mansimran2001@googlemail.com>
2023-06-20 15:53:55 +0100

    usercontroller errors

--- src/main/java/com/trading/entities/User.java
+++ src/main/java/com/trading/entities/User.java
@@ -6,17 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
-//test
 @Entity
 @Table(name="user")
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 public class User {
 
 	@Id
