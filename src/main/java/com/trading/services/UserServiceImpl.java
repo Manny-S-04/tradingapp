8b50e968a5c60cb0bc8571106abc239f271c33ad
Ariana Nelson <arinola540@gmail.com>
2023-06-20 14:17:59 +0100

    user service Impl

--- src/main/java/com/trading/services/UserServiceImpl.java
+++ src/main/java/com/trading/services/UserServiceImpl.java
@@ -1,5 +1,34 @@
 package com.trading.services;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trading.entities.User;
+import com.trading.repositories.UserRepository;
+
+
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	
+	@Override
+	public User registerUser(User user) {
+		
+		return userRepository.save(user);
+	}
+	
+
+	@Override
+	public User loginUser(String username, String password) {
+		
+		return userRepository.loginProcess(username, password);
+	}
 
 }
