01f78d13f8c04285a43b762e912a657df81a704a
manny <mansimran2001@googlemail.com>
2023-06-20 14:02:51 +0100

    Added Order class

--- src/main/java/com/trading/services/UserService.java
+++ src/main/java/com/trading/services/UserService.java
@@ -1,10 +1,17 @@
 package com.trading.services;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.trading.entities.User;
 
 public interface UserService {
 
-public User registerUser(User user);
-	
+	public User registerUser(User user);
+
 	public User loginUser(String username, String password);
+
+	@Query("SELECT u FROM User u WHERE u.email = :email")
+	public User findEmail(@Param("email") String email); //to be used when to search if a user already exists when registering 
+
 }
