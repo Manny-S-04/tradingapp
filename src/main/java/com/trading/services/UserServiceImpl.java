7ce3086f1a3662bdb2e9736db467554fe8f033c2
Ariana Nelson <arinola540@gmail.com>
2023-06-20 14:38:14 +0100

    done

--- src/main/java/com/trading/services/UserServiceImpl.java
+++ src/main/java/com/trading/services/UserServiceImpl.java
@@ -33,7 +33,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User loginUser(String username, String password) {
 		
-		return userRepository.loginProcess(username, password);
+		return userRepository.login(username, password);
 	}
 
 	@Override
