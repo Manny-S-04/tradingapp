d4cdfcc69d3e4a1be4207e8d35d5e78d2405009a
manny <mansimran2001@googlemail.com>
2023-06-26 10:17:08 +0100

    OBR

--- src/main/java/com/trading/repositories/OrderBookRepository.java
+++ src/main/java/com/trading/repositories/OrderBookRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.trading.entities.OrderBook;
-import com.trading.entities.User;
 @Repository
 public interface OrderBookRepository extends JpaRepository<OrderBook, Integer>{
+	
 
 }
