cc28e594fd8f82eb6426da0b42b596c13176d1a4
manny <mansimran2001@googlemail.com>
2023-06-26 11:40:26 +0100

    updated repos and order contrller

--- src/main/java/com/trading/services/OrderService.java
+++ src/main/java/com/trading/services/OrderService.java
@@ -2,17 +2,13 @@ package com.trading.services;
 
 import java.util.List;
 
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
 
 import com.trading.entities.Order;
 
+@Service
 public interface OrderService {
 	
-	public Order saveOrder(Order order);
-	
-	public List<Order> partialOrders(int userid);
-	public List<Order> filledOrders(int userid);
-	
-	public void deleteOrder(int userid);
+	public List<Order> findOrders(int userid);
 
 }
--- src/main/java/com/trading/services/OrderServiceImpl.java
+++ src/main/java/com/trading/services/OrderServiceImpl.java
@@ -3,55 +3,22 @@ package com.trading.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.trading.entities.Order;
 import com.trading.repositories.OrderRepository;
 
+@Service
 public class OrderServiceImpl implements OrderService {
 
 	@Autowired 
 	private OrderRepository orderRepository;
-	
-	
-	
-	
-	@Override
-	public Order saveOrder(Order order) {
-		
-		return orderRepository.save(order);
-	}
-
-
-
 
 	@Override
-	public List<Order> partialOrders(int userid) {
-		return orderRepository.partialOrders(userid);
+	public List<Order> findOrders(int userid) {
+		// TODO Auto-generated method stub
+		return orderRepository.findOrders(userid);
 	}
-
-	
-	
-
-
-
-	@Override
-	public List<Order> filledOrders(int userid) {
-		return orderRepository.filledOrders(userid);
-	}
-
-
-
-
-	@Override
-	public void deleteOrder(int userid) {
-		orderRepository.deleteOrder(userid);
-		
-	}
-
-
-
-
-	
 	
 	
 
